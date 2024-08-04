124ccd80648f02527c6ec2929d69f92d6b474c43
Muhammadcoder2001 <abduqahhorovmuhammadqobil@gmail.com>
2024-08-04 16:15:14 +0500

    Task 17

---
+++ Task18.java
@@ -0,0 +1,35 @@
+package Collections;
+
+import java.util.ArrayList;
+
+public class Task18 {
+
+    public static void main(String[] args) {
+
+        ArrayList <String> list = new ArrayList<>();
+        ArrayList <Integer> list1 = new ArrayList<>();
+
+        list.add("Abdulloh");
+        list.add("Hafizullo");
+        list.add("Sardor");
+        list.add("Abdulbosit");
+        list.add("Habibullo");
+        list.add("Samandar");
+
+        if(list.isEmpty()){
+            System.out.println("List is empty");
+        }
+        else{
+            System.out.println("List is not empty");
+        }
+        if(list1.isEmpty()){
+            System.out.println("List is empty");
+        }
+        else{
+            System.out.println("List is not empty");
+
+        }
+
+
+    }
+}
